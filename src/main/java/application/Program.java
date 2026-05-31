2b4f8c530ed89ee1625f17873570615112766a3f
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-05-31 18:31:10 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
   public static void main(String[] args) {
     Department department = new Department(1, "testes");
     System.out.println(department);
+
+    Seller seller = new Seller(21, "Bobo", "boobbb", new Date(), 3000.0, department);
+    System.out.println("\n" + seller);
+
   }
 }
